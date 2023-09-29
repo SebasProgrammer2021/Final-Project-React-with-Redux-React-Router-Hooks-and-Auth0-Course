cd0a4e6d98196283905397f897f252a383505285
SebasProgrammer2021 <sebastian.londonov@uqvirtual.edu.co>
2023-09-28 19:37:23 -0500

    adelanto del curso

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# react-redux-app-sample
+# Proyecto final del curso
+
+* idioma del curso ingles
+enlace del curso [React with redux](https://www.udemy.com/course/modern-react/learn/lecture/13926064#overview)
+
--- src/hooks/hooks_container1.js
+++ src/hooks/hooks_container1.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
+import * as Reducer from '../store/hooks_state/hooks_reducer'
 
 const HooksContainer1 = () => {
+  const [stateValue, setStateValue] = useState(0)
+  const [useEffectValue, setUseEffectValue] = useState(null);
+  const [state, dispatch] = useReducer(reducer, initial)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setUseEffectValue("useeffect worked")
+    }, 3000);
+  }, [stateValue]);
+
+  const incrementValue = () => {
+    setStateValue(stateValue + 1)
+  }
+
+  const decrementValue = () => {
+    setStateValue(stateValue - 1)
+  }
+
+  const changeUseEffectValue = () => {
+    setUseEffectValue('Changed value')
+  }
+
   return (
     <div>
       React hookscontainer
+      <br />
+      <button onClick={() => incrementValue()}>
+        increment local state
+      </button>
+      <button onClick={() => decrementValue()}>
+        decrement local state
+      </button>
+      <button onClick={() => changeUseEffectValue()}>
+        change UseEffect Value
+      </button>
+      <br />
+      <div>
+        <p>
+          {useEffectValue ?
+            <p>{useEffectValue}</p>
+            : <p>No value</p>
+          }
+        </p>
+        <p>
+          local state: {stateValue}
+        </p>
+      </div>
     </div>
   )
 }
